21a70735c5868e00c050dfd89676e005df66a4d7
ohtaekwon <ohtk1119@gmail.com>
2022-10-28 16:12:53 +0900

    chore: 아폴로 서버로 교체

--- server/src/index.js
+++ server/src/index.js
@@ -1,39 +1,60 @@
 import express from "express";
-import cors from "cors";
-import messagesRoute from "./routes/messages.js";
-import usersRoute from "./routes/users.js";
+import { ApolloServer } from "apollo-server-express";
+import { readDB } from "./dbController.js";
+import resolvers from "./resolvers/index.js";
+import schema from "./schema/index.js";
 
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+  context: {
+    // resolver들이 참조할 데이터
+    models: {
+      messages: readDB("messages"),
+      users: readDB("users"),
+    },
+  },
+});
 const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(
-  cors({
-    origin: "http://localhost:3000", // 클라이언트 서버
+await server.start();
+server.applyMiddleware({
+  app,
+  path: "/graphql",
+  cors: {
+    origin: ["http://localhost:3000", "https://studio.apollographql.com"], // 클라이언트 서버
     credentials: true,
-  })
-);
-// app[method](route, handler)
-
-const routes = [...messagesRoute, ...usersRoute];
-
-routes.forEach(({ method, route, handler }) => {
-  app[method](route, handler);
+  },
 });
 
-app.listen(8000, () => {
-  console.log("server listening on 8000");
-});
+await app.listen(8000);
+console.log("server listening on 8000");
 
-// app.get("/", (req, res) => {
-//   res.send("ok");
-// });
-// app.post('/messages',(req,res)=>{
-//   ...
+// 교체
+
+// import express from 'express'
+// import cors from 'cors'
+// import messagesRoute from './routes/messages.js'
+// import usersRoute from './routes/users.js'
+
+// const app = express()
+/* apollo server사용시 필요없어짐 */
+// app.use(express.urlencoded({ extended: true }))
+// app.use(express.json())
+
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     credentials: true,
+//   }),
+// )
+
+// // app[method](route, handler)
+
+// const routes = [...messagesRoute, ...usersRoute]
+// routes.forEach(({ method, route, handler }) => {
+//   app[method](route, handler)
 // })
-// app.put('/messages/:id',(req,res)=>{
-//   ....
-// })
-// app.delete('/messages/:id',(req,res)=>{
-//   ....
+
+// app.listen(8000, () => {
+//   console.log('server listening on 8000...')
 // })
