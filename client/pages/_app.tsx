26f18ecd7dafeda1e7425f56f1a17c112e6b35d1
ohtaekwon <ohtk1119@gmail.com>
2022-10-30 18:06:56 +0900

    chore: 쿼리 클라이언트 연결

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,9 +1,32 @@
-import React, { FC, ReactNode, useEffect } from "react";
+import React, { useRef } from "react";
 import "./index.scss";
+import { QueryClient, QueryClientProvider } from "react-query";
+import {Hydrate} from 'react-query/hydration'
 import type { AppProps } from "next/app";
 import GlobalStyles from "../styles/Globalstyle";
 
-const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+const App = ({ Component, pageProps }) => {
+  const clientRef = useRef(null);
+  const getClient = () => {
+    if (!clientRef.current)
+      clientRef.current = new QueryClient({
+        defaultOptions: {
+          queries: {
+            //  refetchInterval : 10000,
+            refetchOnWindowFocus: false,
+          },
+        },
+      });
+    return clientRef.current;
+  };
+  return (
+    <QueryClientProvider client={getClient()}>
+      <Hydrate state={pageProps.dehydratedState}>
+      <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
+};
 
 App.getInitialProps = async ({ ctx, Component }) => {
   const pageProps = await Component.getInitialProps?.(ctx);
