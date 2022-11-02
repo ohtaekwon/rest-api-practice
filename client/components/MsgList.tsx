708681983ddcf71e1056ac0903fe555fe44783b7
ohtaekwon <ohtk1119@gmail.com>
2022-11-03 05:40:48 +0900

    초기 데이터 호출 구현

--- client/components/MsgList.tsx
+++ client/components/MsgList.tsx
@@ -2,32 +2,26 @@ import React, { FC, useEffect, useState } from "react";
 import styled from "styled-components";
 
 import MsgItem from "./MsgItem";
+import MsgInput from "./MsgInput";
+
+import fetcher from "../fetcher";
+
 import { MessageType } from "../types/messages";
 import { UsersType } from "../types/users";
-import MsgInput from "./MsgInput";
 
 const UserIds = ["roy", "jay"];
 const getRandomUserId = () => UserIds[Math.round(Math.random())];
-const originalMsgs = Array(50)
-  .fill(0)
-  .map((_, index) => ({
-    id: index + 1,
-    userId: getRandomUserId,
-    timestamp: 1234567890123 + index * 1000 * 60,
-    text: `${index + 1} mock text`,
-  }))
-  .reverse();
 
 type Message = {
   id: number;
-  userId: () => string;
+  userId: string;
   timestamp: number;
   text: string;
 };
 
 type Props = {};
 const MsgList: FC<Props> = (): JSX.Element => {
-  const [msgs, setMsgs] = useState<Message[] | any>(originalMsgs); // 변경을 감지하기 위해서 state를 이용한다.
+  const [msgs, setMsgs] = useState<Message[]>([]); // 변경을 감지하기 위해서 state를 이용한다.
   const [editingId, setEditingId] = useState(null);
 
   const onCrate = (text) => {
@@ -65,7 +59,15 @@ const MsgList: FC<Props> = (): JSX.Element => {
       return newMsgs;
     });
   };
-  console.log(JSON.stringify(msgs));
+  const getMessages = async () => {
+    const msgs = await fetcher("get", "/messages");
+    setMsgs(msgs);
+  };
+
+  useEffect(() => {
+    getMessages();
+  }, []);
+
   return (
     <>
       <MsgInput mutate={onCrate} />
